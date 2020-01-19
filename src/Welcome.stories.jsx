836e987761c65a8fd08cd7b617939cf02e065622
Luis Silva <luis.silva@dal.ca>
2020-01-19 14:07:24 -0500

    Add welcome page bare bones

---
+++ src/Welcome.stories.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default {
+  title: "Welcome|Getting Started"
+};
+
+export const usage = () => (
+  <div>
+    Welcome
+    <div>
+      Importing material components while minimizing bundle size:
+      https://material-ui.com/guides/minimizing-bundle-size/{" "}
+    </div>
+  </div>
+);
