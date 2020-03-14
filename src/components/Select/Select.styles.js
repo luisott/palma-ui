7c5862f9b81bbf7dba0bd039dee56991c8162707
Luis Silva <luis.silva@dal.ca>
2020-03-14 13:04:46 -0400

    Fix old style

--- src/components/Select/Select.styles.js
+++ src/components/Select/Select.styles.js
@@ -1,9 +1,11 @@
 import { css } from "@emotion/core";
 
 export const selectStyle = css`
-  height: 40px;
+  && {
+    height: 40px;
 
-  & div {
-    background-color: unset;
+    & div {
+      background-color: unset;
+    }
   }
 `;
