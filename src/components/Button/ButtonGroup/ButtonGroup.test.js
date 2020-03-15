41bbf467154ac896fc51c9b3b8cdcbc2adec780c
Luis Silva <luis.silva@dal.ca>
2020-03-15 12:43:04 -0400

    Fix tests

--- src/components/Button/ButtonGroup/ButtonGroup.test.js
+++ src/components/Button/ButtonGroup/ButtonGroup.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import ButtonGroup from "./ButtonGroup";
+import { ButtonGroup } from "./ButtonGroup";
 import { Button } from "../Button";
 
 it("renders some label", () => {
