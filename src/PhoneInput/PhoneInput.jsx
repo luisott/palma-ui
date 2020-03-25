b6603e2d608e6297ecc75aa1558e6ef1b943bfc6
Luis Silva <luis.silva@dal.ca>
2020-03-25 16:10:38 -0400

    Controlled phone number input

--- src/PhoneInput/PhoneInput.jsx
+++ src/PhoneInput/PhoneInput.jsx
@@ -8,9 +8,12 @@ import { MenuItem } from "../MenuItem";
 import { InputGroup, InputGroupInput, InputGroupSelect } from "../InputGroup";
 import { getMenuProps } from "../Menu";
 
-// TODO: Add label
 // TODO: MAke uncontrolled
 // TODO: Format north american numbers with (XXX) XXX-XXXX and all others with XXXXXXXXXXXXXX
+// TODO: Remove hardcoded strings for labels etc.
+// TODO: Pass down options to do dense padding in both here and input group
+
+const DEFAULT_COUNTRY_ID = "CA";
 
 const countriesWithFlagsAndIds = allCountries.map(country => ({
   ...country,
@@ -18,20 +21,68 @@ const countriesWithFlagsAndIds = allCountries.map(country => ({
   id: country.code
 }));
 
+const getCountryFromNumber = (
+  value,
+  preferredCountryId = DEFAULT_COUNTRY_ID
+) => {
+  const justDigits = getCleanNumber(value);
+  if (justDigits === "") {
+    return getCountryWithId(preferredCountryId);
+  }
+  const countriesWithCode = countriesWithFlagsAndIds.filter(({ phone }) =>
+    justDigits.startsWith(phone)
+  );
+  if (countriesWithCode.length > 1 && preferredCountryId) {
+    const preferredCountry = countriesWithCode.find(
+      ({ id }) => id === preferredCountryId
+    );
+    if (preferredCountry) {
+      return preferredCountry;
+    }
+  }
+  return countriesWithCode[0];
+};
+
+const getCountryWithId = countryId =>
+  countriesWithFlagsAndIds.find(({ id }) => id === countryId);
+
+const getCleanNumber = phoneNumber => {
+  return phoneNumber ? phoneNumber.replace(/\D/g, "") : "";
+};
+
+const getPhoneWithoutCountryCode = value => {
+  const cleanNumber = getCleanNumber(value);
+  const country = getCountryFromNumber(value);
+  if (country) {
+    return cleanNumber.replace(country.phone, "");
+  }
+  return cleanNumber;
+};
+
 const propTypes = {
-  someBoolean: PropTypes.bool,
-  defaultCountryId: PropTypes.string
+  defaultCountryId: PropTypes.string,
+  label: PropTypes.string,
+  disabled: PropTypes.bool,
+  phoneNumber: PropTypes.string,
+  onChange: PropTypes.func.isRequired
 };
 
 const defaultProps = {
-  defaultCountryId: "CA"
+  defaultCountryId: DEFAULT_COUNTRY_ID
 };
 
-const PhoneInput = ({ defaultCountryId }) => {
-  const getSelectedCountry = countryId => {
-    const selectedCountry = countriesWithFlagsAndIds.find(
-      ({ id }) => id === countryId
-    );
+const PhoneInput = ({
+  defaultCountryId,
+  label,
+  phoneNumber,
+  onChange,
+  disabled
+}) => {
+  const country = getCountryFromNumber(phoneNumber);
+  const localNumber = getPhoneWithoutCountryCode(phoneNumber);
+
+  const renderSelectedCountry = countryId => {
+    const selectedCountry = getCountryWithId(countryId);
     if (!selectedCountry) {
       return null;
     }
@@ -52,6 +103,14 @@ const PhoneInput = ({ defaultCountryId }) => {
     ));
   };
 
+  const handleCountryChange = event => {
+    onChange(`${event.target.value}${localNumber}`);
+  };
+
+  const handleLocalPhoneNumberChange = event => {
+    onChange(`${country.phone}${event.target.value}`);
+  };
+
   const commonMenuProps = getMenuProps(false, "left");
   const menuProps = {
     ...commonMenuProps,
@@ -59,20 +118,26 @@ const PhoneInput = ({ defaultCountryId }) => {
   };
 
   return (
-    <InputGroup>
+    <InputGroup label={label} disabled={disabled}>
       <InputGroupSelect
         id={"country picker"}
         labelId={"country picker"}
-        renderValue={getSelectedCountry}
+        renderValue={renderSelectedCountry}
         MenuProps={menuProps}
         defaultValue={defaultCountryId}
         css={styles.menu}
+        disabled={disabled}
+        value={country?.id}
+        onChange={handleCountryChange}
       >
         {getOptions()}
       </InputGroupSelect>
-      <InputGroupInput id={"some-id"} placeholder={"Placeholder"}>
-        <div>asdasdad</div>
-      </InputGroupInput>
+      <InputGroupInput
+        id={"number"}
+        disabled={disabled}
+        value={localNumber}
+        onChange={handleLocalPhoneNumberChange}
+      />
     </InputGroup>
   );
 };
--- src/PhoneInput/PhoneInput.stories.jsx
+++ src/PhoneInput/PhoneInput.stories.jsx
@@ -1,11 +1,20 @@
-import React from "react";
-// import { text, boolean, select } from "@storybook/addon-knobs";
-import {PhoneInput} from "./PhoneInput";
+import React, { useState } from "react";
+import { text, boolean } from "@storybook/addon-knobs";
+import { PhoneInput } from "./PhoneInput";
 
 export default {
-    title: "Components/PhoneInput"
+  title: "Components/PhoneInput"
 };
 
-export const phoneInput = () => (
-    <PhoneInput/>
-);
+export const PhoneInputControlled = () => {
+  const [phoneNumber, setPhoneNumber] = useState("16137775454");
+
+  return (
+    <PhoneInput
+      label={text("Label", "Enter phone number")}
+      disabled={boolean("Disabled", false)}
+      phoneNumber={phoneNumber}
+      onChange={newNumber => setPhoneNumber(newNumber)}
+    />
+  );
+};
