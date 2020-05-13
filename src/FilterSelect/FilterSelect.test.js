4c6d692037bae20713c7d63d8e3519ca24cf9cfa
Luis Silva <luis.silva@dal.ca>
2020-05-12 22:56:47 -0400

    New version

--- src/FilterSelect/FilterSelect.test.js
+++ src/FilterSelect/FilterSelect.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { fireEvent, render } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import { FilterSelect } from "./FilterSelect";
 
 const movies = [
@@ -11,44 +11,17 @@ const movies = [
   { name: "Inglourious Basterds", year: 2009, id: "5" },
   { name: "Snatch", year: 2000, id: "6" },
   { name: "3 Idiots", year: 2009, id: "7" },
-  { name: "Monty Python and the Holy Grail", year: 1975, id: "8" }
+  { name: "Monty Python and the Holy Grail", year: 1975, id: "8" },
 ];
 
-it("renders empty by default", () => {
+it("renders label", () => {
   const { getByLabelText } = render(
     <FilterSelect
       options={movies}
       label={"some label"}
-      dropDownIconLabel={"show options"}
       showLabel={true}
+      inputProps={{ placeholder: "search stuff" }}
     />
   );
-  expect(getByLabelText("show options")).toBeInTheDocument();
   expect(getByLabelText("some label")).toBeInTheDocument();
-});
-
-// The useRef mock below throws an error
-it.skip("renders options when arrow button clicked", () => {
-  jest.spyOn(React, "useRef").mockReturnValueOnce({ current: jest.fn() });
-
-  const { getByLabelText, getByText } = render(
-    <FilterSelect
-      options={movies}
-      label={"some label"}
-      dropDownIconLabel={"show options"}
-    />
-  );
-  fireEvent.click(getByLabelText("show options"));
-  expect(getByText(movies[0].name)).toBeInTheDocument();
-});
-
-it("no drop down arrow if no options passed", () => {
-  const { queryByLabelText } = render(
-    <FilterSelect
-      options={[]}
-      label={"some label"}
-      dropDownIconLabel={"show options"}
-    />
-  );
-  expect(queryByLabelText("show options")).not.toBeInTheDocument();
 });
--- src/SearchBar/SearchBar.test.js
+++ src/SearchBar/SearchBar.test.js
@@ -4,15 +4,7 @@ import { SearchBar } from "./SearchBar";
 
 it("renders label", () => {
   const label = "label";
-  const { getByLabelText } = render(
-    <SearchBar
-      label={label}
-      value={""}
-      onClickAway={() => {
-        return;
-      }}
-    />
-  );
+  const { getByLabelText } = render(<SearchBar label={label} value={""} />);
   expect(getByLabelText(label)).toBeInTheDocument();
 });
 
@@ -20,31 +12,8 @@ it("renders placeholder and no close button when empty", () => {
   const label = "label";
   const placeholder = "placeholder";
   const { getByPlaceholderText, queryByLabelText } = render(
-    <SearchBar
-      label={label}
-      placeholder={placeholder}
-      value={""}
-      onClickAway={() => {
-        return;
-      }}
-    />
+    <SearchBar label={label} placeholder={placeholder} value={""} />
   );
   expect(getByPlaceholderText(placeholder)).toBeInTheDocument();
   expect(queryByLabelText("close")).not.toBeInTheDocument();
-});
-
-it("renders value and close button when value changes to not empty", () => {
-  const label = "label";
-  const value = "value";
-  const { getByLabelText } = render(
-    <SearchBar
-      onClickAway={() => {
-        return;
-      }}
-      label={label}
-    />
-  );
-
-  fireEvent.change(getByLabelText(label), { target: { value: value } });
-  expect(getByLabelText("close")).toBeInTheDocument();
 });
